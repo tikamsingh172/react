296fa3e9bf821a02fa35ae4e4aa7524e6da36721
tikamsingh172 <tikamsingh172@gmail.com>
2024-03-19 20:36:28 +0530

    file-cleanup

--- 01-vite-react/src/components/User.jsx
+++ 01-vite-react/src/components/User.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function User() {
     return (
         <div>
-            <h1>Hello User...</h1>
+            <h1>Hello User... From vite-react-app</h1>
         </div>
     )
 }
